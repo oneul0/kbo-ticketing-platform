2f828ecbb3e75bae2dbff20fe6c0afd2048948e6
ybon1107 <leeyebon7929@gmail.com>
2025-04-07 22:20:05 +0900

    feat: eureka - eureka service add#8
    - application.yml 은 따로 공유 예정

---
+++ eureka-service/src/test/java/com/boeingmerryho/infrastructure/eurekaservice/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boeingmerryho.infrastructure.eurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
