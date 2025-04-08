574ba7cce105c61891111ad9335c361df7489a95
juneunbae <dmsqo1403@naver.com>
2025-04-09 02:24:43 +0900

    [Seat] : ADMIN - 좌석 생성하기 #24
    1. SeatAdminController
    - (ADMIN - 좌석 추가하기) createSeat 메서드 추가
    
    2. SeatAdminService
    - createSeat 메서드 추가
    - makeSeatNo 메서드 추가
    
    3. SeatCreateRequestDto
    - Presentation Request Dto 추가
    
    4. SeatCreateResponseDto
    - Presentation Response Dto 추가
    
    5. SeatCreateServiceRequestDto
    - Application Request Dto 추가
    
    6. SeatCreateServiceResponseDto
    - Application Response Dto 추가
    
    7. SeatPresentationMapper
    - toSeatCreateResponseDto 메서드 추가
    - toSeatCreateServiceRequestDto 메서드 추가
    
    8. SeatApplicationMapper
    - toSeatCreateServiceResponseDto 메서드 추가
    
    9. SeatRepository
    
    10. SeatSuccessCode
    - OK_SEAT
    - CREATED_SEAT
    - UPDATED_SEAT
    - DELETED_SEAT

--- seat-service/src/main/java/com/boeingmerryho/business/seatservice/application/dto/mapper/SeatApplicationMapper.java
+++ seat-service/src/main/java/com/boeingmerryho/business/seatservice/application/dto/mapper/SeatApplicationMapper.java
@@ -2,6 +2,10 @@ package com.boeingmerryho.business.seatservice.application.dto.mapper;
 
 import org.mapstruct.Mapper;
 
+import com.boeingmerryho.business.seatservice.application.dto.response.SeatCreateServiceResponseDto;
+import com.boeingmerryho.business.seatservice.domain.Seat;
+
 @Mapper(componentModel = "spring")
 public interface SeatApplicationMapper {
+	SeatCreateServiceResponseDto toSeatCreateServiceResponseDto(Seat seat);
 }
