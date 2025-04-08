f46572795c62dd384654b91969f953b165055663
juneunbae <dmsqo1403@naver.com>
2025-04-09 02:10:41 +0900

    [Seat] : ADMIN - 좌석 생성하기 #24
    1. BaseEntity 삭제
    
    2. build.gradle
    - common library 적용
    
    3. SeatReservation
    - BaseEntity (common library 내 BaseEntity 설정)

--- seat-service/src/main/java/com/boeingmerryho/business/seatservice/domain/SeatReservation.java
+++ seat-service/src/main/java/com/boeingmerryho/business/seatservice/domain/SeatReservation.java
@@ -1,14 +1,23 @@
 package com.boeingmerryho.business.seatservice.domain;
 
-import jakarta.persistence.*;
+import java.time.LocalDate;
+
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import io.github.boeingmerryho.commonlibrary.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
-import org.hibernate.annotations.DynamicInsert;
-import org.hibernate.annotations.DynamicUpdate;
-
-import java.time.LocalDate;
 
 @Getter
 @Entity
@@ -18,27 +27,25 @@ import java.time.LocalDate;
 @Table(name = "p_seat_reservation")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class SeatReservation extends BaseEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    @Column(
-        nullable = false
-    )
-    private Long seatId;
+	@ManyToOne(fetch = FetchType.LAZY)
+	private Seat seat;
 
-    @Column(
-        nullable = false
-    )
-    private Long userId;
+	@Column(
+		nullable = false
+	)
+	private Long userId;
 
-    @Column(
-        nullable = false
-    )
-    private LocalDate reservationDate;
+	@Column(
+		nullable = false
+	)
+	private LocalDate reservationDate;
 
-    @Column(
-        nullable = false
-    )
-    private Boolean isReserved;
+	@Column(
+		nullable = false
+	)
+	private Boolean isReserved;
 }
