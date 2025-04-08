83e82fbb7fd61b92302db7d38742da67b3bb1dc5
haisley77 <hahi1108@gmail.com>
2025-04-08 20:17:11 +0900

    feat: payment - 결제내역 조회(admin)#25
    - controller
    - service

---
+++ payment-service/src/main/java/com/boeingmerryho/business/paymentservice/presentation/PaymentAdminController.java
@@ -0,0 +1,33 @@
+package com.boeingmerryho.business.paymentservice.presentation;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.boeingmerryho.business.paymentservice.application.PaymentAdminService;
+import com.boeingmerryho.business.paymentservice.application.dto.response.PaymentDetailResponseServiceDto;
+import com.boeingmerryho.business.paymentservice.presentation.dto.response.PaymentDetailResponseDto;
+import com.boeingmerryho.business.paymentservice.presentation.temp.PaymentSuccessCode;
+import com.boeingmerryho.business.paymentservice.presentation.temp.SuccessResponse;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/admin/v1/payments")
+@RequiredArgsConstructor
+public class PaymentAdminController {
+	private final PaymentAdminService paymentAdminService;
+	private final PaymentPresentationMapper mapper;
+
+	@GetMapping("/details/{id}")
+	public ResponseEntity<SuccessResponse<PaymentDetailResponseDto>> getPaymentDetail(@PathVariable Long id) {
+
+		PaymentDetailResponseServiceDto responseServiceDto = paymentAdminService.getPaymentDetail(
+			mapper.toPaymentDetailRequestServiceDto(id));
+		return SuccessResponse.of(PaymentSuccessCode.FETCHED_PAYMENT_DETAIL,
+			mapper.toPaymentDetailResponseDto(responseServiceDto));
+
+	}
+}
