2865df4769e3dfafed61cc06816761df18005998
ybon1107 <leeyebon7929@gmail.com>
2025-04-09 02:02:01 +0900

    fix: store - mapper bean 중복 등록 제거#21

--- store-service/src/main/java/com/boeingmerryho/business/storeservice/application/dto/mapper/StoreApplicationMapper.java
+++ store-service/src/main/java/com/boeingmerryho/business/storeservice/application/dto/mapper/StoreApplicationMapper.java
@@ -3,11 +3,13 @@ package com.boeingmerryho.business.storeservice.application.dto.mapper;
 import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.springframework.context.annotation.Primary;
 
 import com.boeingmerryho.business.storeservice.application.dto.request.StoreCreateRequestServiceDto;
 import com.boeingmerryho.business.storeservice.application.dto.response.StoreCreateResponseServiceDto;
 import com.boeingmerryho.business.storeservice.domain.entity.Store;
 
+@Primary
 @Mapper(componentModel = "spring")
 public interface StoreApplicationMapper {
 
--- store-service/src/main/java/com/boeingmerryho/business/storeservice/presentation/dto/mapper/StorePresentationMapper.java
+++ store-service/src/main/java/com/boeingmerryho/business/storeservice/presentation/dto/mapper/StorePresentationMapper.java
@@ -1,12 +1,14 @@
 package com.boeingmerryho.business.storeservice.presentation.dto.mapper;
 
 import org.mapstruct.Mapper;
+import org.springframework.context.annotation.Primary;
 
 import com.boeingmerryho.business.storeservice.application.dto.request.StoreCreateRequestServiceDto;
 import com.boeingmerryho.business.storeservice.application.dto.response.StoreCreateResponseServiceDto;
 import com.boeingmerryho.business.storeservice.presentation.dto.request.StoreCreateRequestDto;
 import com.boeingmerryho.business.storeservice.presentation.dto.response.StoreCreateResponseDto;
 
+@Primary
 @Mapper(componentModel = "spring")
 public interface StorePresentationMapper {
 	StoreCreateRequestServiceDto toStoreCreateRequestServiceDto(StoreCreateRequestDto dto);
