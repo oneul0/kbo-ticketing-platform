6e8c94957e299a72ce20ff69bd42f045eb9954f6
oneul0 <neul.dev@gmail.com>
2025-04-10 12:08:32 +0900

    feat: user - intercepter config 추가#28

---
+++ user-service/src/main/java/com/boeingmerryho/business/userservice/config/interceptor/WebMvcConfig.java
@@ -0,0 +1,32 @@
+package com.boeingmerryho.business.userservice.config.interceptor;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+	private final RedisTemplate<String, Object> redisTemplate;
+
+	public WebMvcConfig(RedisTemplate<String, Object> redisTemplate) {
+		this.redisTemplate = redisTemplate;
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		// registry.addInterceptor(new AdminCheckInterceptor(redisTemplate))
+		// 	.excludePathPatterns(
+		// 			"/api/**",
+		// 			"/error/**"
+		// 	);
+		//
+		// registry.addInterceptor(new UserCheckInterceptor(redisTemplate))
+		// 	.excludePathPatterns(
+		// 			"/admin/**",
+		// 			"/error/**"
+		// 	);
+	}
+
+}
