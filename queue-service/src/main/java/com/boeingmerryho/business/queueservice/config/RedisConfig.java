7b18b0adaa55758af0fca58265fe2d34a1a54e66
oneul0 <neul.dev@gmail.com>
2025-04-13 16:17:40 +0900

    feat: queue - 분산락을 위한 redisson, aop 설정#75

--- queue-service/src/main/java/com/boeingmerryho/business/queueservice/config/RedisConfig.java
+++ queue-service/src/main/java/com/boeingmerryho/business/queueservice/config/RedisConfig.java
@@ -1,5 +1,8 @@
 package com.boeingmerryho.business.queueservice.config;
 
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -28,6 +31,17 @@ public class RedisConfig {
 
 	@Value("${spring.data.redis.password}")
 	private String password;
+
+	private static final String REDISSON_HOST_PREFIX = "redis://";
+
+	@Bean
+	public RedissonClient redissonClient() {
+		RedissonClient redisson = null;
+		Config config = new Config();
+		config.useSingleServer().setAddress(REDISSON_HOST_PREFIX + host + ":" + port);
+		redisson = Redisson.create(config);
+		return redisson;
+	}
 
 	@Bean
 	public RedisConnectionFactory redisConnectionFactory() {
--- queue-service/src/main/java/com/boeingmerryho/business/queueservice/infrastructure/QueueJoinHelperImpl.java
+++ queue-service/src/main/java/com/boeingmerryho/business/queueservice/infrastructure/QueueJoinHelperImpl.java
@@ -1,5 +1,6 @@
 package com.boeingmerryho.business.queueservice.infrastructure;
 
+import java.time.Duration;
 import java.time.LocalDate;
 import java.util.Date;
 import java.util.Objects;
@@ -10,7 +11,6 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
 import com.boeingmerryho.business.queueservice.application.QueueJoinHelper;
-import com.boeingmerryho.business.queueservice.application.utils.RedisUtil;
 import com.boeingmerryho.business.queueservice.exception.ErrorCode;
 
 import io.github.boeingmerryho.commonlibrary.exception.GlobalException;
@@ -20,10 +20,12 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class QueueJoinHelperImpl implements QueueJoinHelper {
 
+	//todo: value로 주입받기
 	private static final String MEMBERSHIP_INFO_PREFIX = "user:membership:info:";
 	private static final String TICKET_INFO_PREFIX = "queue:ticket:";
 	private static final String STORE_AVAILABILITY_CHECK_PREFIX = "queue:availability:";
 	private static final String WAITLIST_INFO_PREFIX = "queue:waitlist:store:";
+	private static final Long WAITLIST_INFO_EXPIRE_DAY = 1L;
 
 
 	private final RedisTemplate<String, Object> redisTemplate;
@@ -51,19 +53,27 @@ public class QueueJoinHelperImpl implements QueueJoinHelper {
 		String queueKey = WAITLIST_INFO_PREFIX + storeId + ":" + today;
 		String seqKey = queueKey + ":seq";
 
-		try{
+		try {
 			Long order = redisTemplate.opsForValue().increment(seqKey);
 
 			redisTemplate.opsForZSet().add(queueKey, userId.toString(), Objects.requireNonNull(order).doubleValue());
-		}
-		catch (RedisConnectionFailureException e){
+		} catch (RedisConnectionFailureException e) {
 			throw new GlobalException(ErrorCode.QUEUE_JOIN_FAIL);
 		}
 
+		setExpireIfAbsent(queueKey, Duration.ofDays(WAITLIST_INFO_EXPIRE_DAY));
+		setExpireIfAbsent(seqKey, Duration.ofDays(WAITLIST_INFO_EXPIRE_DAY));
 	}
 
 	@Override
 	public Integer getUserQueuePosition(Long storeId, Long userId) {
 		return 0;
+	}
+
+	private void setExpireIfAbsent(String key, Duration ttl) {
+		Long expire = redisTemplate.getExpire(key);
+		if (expire == null || expire == -1) {
+			redisTemplate.expire(key, ttl);
+		}
 	}
 }
