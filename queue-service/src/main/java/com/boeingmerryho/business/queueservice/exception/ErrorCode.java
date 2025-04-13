8fb90edd096d8225f832d1cd41d8189c0f59dcfc
oneul0 <neul.dev@gmail.com>
2025-04-13 15:14:51 +0900

    feat: queue - 대기열 등록 api 구현#75
    - 대기열 등록 시 queueKey, seqKey 두 개의 row로 구성
      - 날짜별로 시퀀스를 관리하는 seq, 실제 대기열에 등록될 정보인 queue로 나누어 관리

--- queue-service/src/main/java/com/boeingmerryho/business/queueservice/exception/ErrorCode.java
+++ queue-service/src/main/java/com/boeingmerryho/business/queueservice/exception/ErrorCode.java
@@ -9,7 +9,11 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum ErrorCode implements BaseErrorCode {
+	STORE_IS_NOT_ACTIVATED("Q-001", "등록하려는 가게가 영업 중이 아닙니다.", HttpStatus.BAD_REQUEST),
+	TICKET_IS_NOT_ACTIVATED("Q-002", "티켓이 유효하지 않습니다.", HttpStatus.BAD_REQUEST),
+	USER_IS_NOT_MATCHED("Q-003", "사용자 정보가 일치하지 않습니다.", HttpStatus.BAD_REQUEST),
 
+	QUEUE_JOIN_FAIL("Q-004", "알 수 없는 오류로 대기열 등록에 실패했습니다.", HttpStatus.BAD_REQUEST),
 	;
 
 	private final String errorCode;
---
+++ queue-service/src/main/java/com/boeingmerryho/business/queueservice/infrastructure/QueueJoinHelperImpl.java
@@ -0,0 +1,70 @@
+package com.boeingmerryho.business.queueservice.infrastructure;
+
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.Objects;
+import java.util.Optional;
+
+import org.springframework.data.redis.RedisConnectionFailureException;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+import com.boeingmerryho.business.queueservice.application.QueueJoinHelper;
+import com.boeingmerryho.business.queueservice.application.utils.RedisUtil;
+import com.boeingmerryho.business.queueservice.exception.ErrorCode;
+
+import io.github.boeingmerryho.commonlibrary.exception.GlobalException;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class QueueJoinHelperImpl implements QueueJoinHelper {
+
+	private static final String MEMBERSHIP_INFO_PREFIX = "user:membership:info:";
+	private static final String TICKET_INFO_PREFIX = "queue:ticket:";
+	private static final String STORE_AVAILABILITY_CHECK_PREFIX = "queue:availability:";
+	private static final String WAITLIST_INFO_PREFIX = "queue:waitlist:store:";
+
+
+	private final RedisTemplate<String, Object> redisTemplate;
+	private final RedisUtil redisUtil;
+
+	@Override
+	public Boolean validateStoreIsActive(Long storeId) {
+		return redisTemplate.hasKey(STORE_AVAILABILITY_CHECK_PREFIX + storeId);
+	}
+
+	@Override
+	public Long validateTicket(Date matchDate, Long ticketId) {
+		String ticketKey = TICKET_INFO_PREFIX + matchDate.toString() + ":" + ticketId;
+		Optional<String> userIdValue = Optional.ofNullable(redisTemplate.opsForValue().get(ticketKey))
+			.map(Object::toString);
+		if (!userIdValue.isPresent()) {
+			throw new GlobalException(ErrorCode.TICKET_IS_NOT_ACTIVATED);
+		}
+		return Long.parseLong(userIdValue.get());
+	}
+
+	@Override
+	public void joinUserInQueue(Long storeId, Long userId, Long ticketId) {
+		String today = LocalDate.now().toString();
+
+		String queueKey = WAITLIST_INFO_PREFIX + storeId + ":" + today;
+		String seqKey = queueKey + ":seq";
+
+		try{
+			Long order = redisTemplate.opsForValue().increment(seqKey);
+
+			redisTemplate.opsForZSet().add(queueKey, userId.toString(), Objects.requireNonNull(order).doubleValue());
+		}
+		catch (RedisConnectionFailureException e){
+			throw new GlobalException(ErrorCode.QUEUE_JOIN_FAIL);
+		}
+
+	}
+
+	@Override
+	public Integer getUserQueuePosition(Long storeId, Long userId) {
+		return 0;
+	}
+}
