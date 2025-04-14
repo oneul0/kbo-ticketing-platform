49cdb5dfa3d734d5cb33ad3bc8a6b40dd2f6ced9
juneunbae <dmsqo1403@naver.com>
2025-04-14 15:02:42 +0900

    [Seat] : 좌석 선점 #98
    build.gradle
    - Kafka 설치
    
    SeatController
    - 좌석 선점하기, processBlockSeats
    
    SeatService
    - 좌석 선점하기, processBlockSeats
    
    ProcessBlockSeatsService
    - getMatchInfo
    - getBlockSeats
    - processBlockSeats
    - createCacheBlockKey
    - getBlockSeats
    - parseSeatBucket
    - createLockKey
    - createCacheSeatKey
    
    SeatSuccessCode
    - PROCESS_SEAT 성공 메시지 추가
    - ALREADY_PROCESS_SEAT 성공 메시지 추가
    
    SeatErrorCode
    - NOT_EXCEED_4_SEAT 에러 메시지 추가
    - FAILED_GET_LOCK 에러 메시지 추가
    - FAILED_PROCESS_SEAT 에러 메시지 추가
    
    SeatPresentationMapper
    - toCacheSeatProcessServiceRequestDto
    - toCacheSeatsProcessServiceRequestDto
    
    Presentation Request Dto
    - CacheSeatProcessRequestDto
    - CacheSeatsProcessRequestDto
    
    SeatApplicationMapper
    - toTicketSeatDto
    - toTicketDto
    
    Application request Dto
    - CacheSeatProcessServiceRequestDto
    - CacheSeatsProcessServiceRequestDto
    - ToTicketMatchDto
    - ToTicketSeatDto
    - ToTicketDto

--- seat-service/src/main/java/com/boeingmerryho/business/seatservice/exception/SeatErrorCode.java
+++ seat-service/src/main/java/com/boeingmerryho/business/seatservice/exception/SeatErrorCode.java
@@ -6,7 +6,10 @@ import io.github.boeingmerryho.commonlibrary.exception.BaseErrorCode;
 
 public enum SeatErrorCode implements BaseErrorCode {
 	NOT_FOUND_SEAT(HttpStatus.NOT_FOUND, "SEAT_001", "존재하지 않는 좌석 정보입니다."),
-	NOT_FOUND_BLOCK(HttpStatus.NOT_FOUND, "SEAT_002", "존재하지 않는 블록 정보입니다.");
+	NOT_FOUND_BLOCK(HttpStatus.NOT_FOUND, "SEAT_002", "존재하지 않는 블록 정보입니다."),
+	NOT_EXCEED_4_SEAT(HttpStatus.BAD_REQUEST, "SEAT_003", "4개 이상의 좌석을 예약할 수 없습니다."),
+	FAILED_GET_LOCK(HttpStatus.BAD_REQUEST, "SEAT_004", "락 획득에 실패하였습니다."),
+	FAILED_PROCESS_SEAT(HttpStatus.BAD_REQUEST, "SEAT_005", "좌석 선점에 실패하였습니다.");
 
 	private final HttpStatus status;
 	private final String errorCode;
