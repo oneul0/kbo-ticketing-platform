b95b0f6c445a99468bc88f38e5e6fc28b32baf28
ybon1107 <leeyebon7929@gmail.com>
2025-04-17 10:55:09 +0900

    feat: store - common interceptor 설정#169
    - common-library upgrade
    - WebMvcConfig 설정
    - RedisConfig 추가

---
+++ store-service/src/main/java/com/boeingmerryho/business/storeservice/infrastructure/config/RedisConfig.java
@@ -0,0 +1,8 @@
+package com.boeingmerryho.business.storeservice.infrastructure.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RedisConfig {
+
+}
