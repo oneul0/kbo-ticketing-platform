354416e73ae5b1b49b444ed79956c64f83b782f3
oneul0 <neul.dev@gmail.com>
2025-04-22 00:49:33 +0900

    build: queue - test용 lombok 추가#219

---
+++ queue-service/src/test/java/com/boeingmerryho/business/queueservice/application/QueueServiceConcurrencyTest.java
@@ -0,0 +1,10 @@
+package com.boeingmerryho.business.queueservice.application;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@SpringBootTest
+public class QueueServiceConcurrencyTest {
+}
