acf95d706a90a407e1219c8125f1ce82a880dfc2
JieonLee <dlwldjs7544@naver.com>
2025-04-22 20:46:57 +0900

    refactor: ticket - TicketPayment 정보 TTL 추가 #210

--- ticket-service/src/main/java/com/boeingmerryho/business/ticketservice/infrastructure/repository/TicketPaymentRedisRepository.java
+++ ticket-service/src/main/java/com/boeingmerryho/business/ticketservice/infrastructure/repository/TicketPaymentRedisRepository.java
@@ -1,5 +1,6 @@
 package com.boeingmerryho.business.ticketservice.infrastructure.repository;
 
+import java.time.Duration;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -11,6 +12,7 @@ import com.boeingmerryho.business.ticketservice.domain.repository.TicketPaymentR
 @Repository
 public class TicketPaymentRedisRepository implements TicketPaymentRepository {
 
+	private static final int EXPIRE_MINUTES = 8;
 	private static final String TICKET_PAYMENT_KEY_PREFIX = "ticket:payment:";
 
 	private final RedisTemplate<String, Object> redisTemplate;
@@ -23,6 +25,7 @@ public class TicketPaymentRedisRepository implements TicketPaymentRepository {
 	public void savePaymentInfo(Long userId, Map<String, Object> paymentInfo) {
 		String redisKey = TICKET_PAYMENT_KEY_PREFIX + userId;
 		redisTemplate.opsForHash().putAll(redisKey, paymentInfo);
+		redisTemplate.expire(redisKey, Duration.ofMinutes(EXPIRE_MINUTES));
 	}
 
 	@Override
